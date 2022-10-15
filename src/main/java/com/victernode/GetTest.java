91ac83e880923c5fef80107402b494b01af71cfa
victer <157459949@qq.com>
2022-10-15 11:29:30 +0800

    third commit

--- src/main/java/com/victernode/GetTest.java
+++ src/main/java/com/victernode/GetTest.java
@@ -4,5 +4,6 @@ public class GetTest {
     public static void main(String[] args) {
         System.out.println("git_01");
         System.out.println("get_02");
+        System.out.println("get_03");
     }
 }
